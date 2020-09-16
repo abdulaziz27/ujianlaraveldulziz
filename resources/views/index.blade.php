7618988b9078057a58493db466bfe00a505d6616
abdulaziz27 <abushafir321@gmail.com>
2020-09-16 14:22:40 +0700

    four commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-    <title>Hitung Cepat | Index</title> 
+    <title>Hitung Cepat | Home</title> 
     <style type="text/css">
       @import url('https://fonts.googleapis.com/css?family=Anton');
       .nav-link {
